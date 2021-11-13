f40088d9b04e6516c12c3f28fbef1562e283e484
Koichi Murakami <koichi.murakami@kek.jp>
2021-11-14 07:35:38 +0900

    remove python include

--- config.cmake
+++ config.cmake
@@ -14,13 +14,13 @@ endif()
 set(GEANT4_INSTALL $ENV{HOME}/opt/geant4/11.0
     CACHE STRING "Geant4 installation path")
 
-# Python header file location
-set(PYTHON_INCLUDE_DIR /usr/include/python3.8
-    CACHE STRING "Python include path")
-
 # Pybind11 header files location (pybind11)
 set(PYBIND11_INCLUDE_DIR /usr/include
     CACHE STRING "Pybind11 include path")
+
+# installed with Homebrew in Mac
+#set(PYBIND11_INCLUDE_DIR /opt/homebrew/include
+#    CACHE STRING "Pybind11 include path")
 
 #set(PYBIND11_INCLUDE_DIR /opt
 #    CACHE STRING "Pybind11 include path")
