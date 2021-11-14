bdbe87d02cadd92df716082d64f6450d9a41826b
Koichi Murakami <koichi.murakami@kek.jp>
2021-11-14 09:22:40 +0900

    add homebrew path

--- config.cmake
+++ config.cmake
@@ -18,9 +18,5 @@ set(GEANT4_INSTALL $ENV{HOME}/opt/geant4/11.0
 set(PYBIND11_INCLUDE_DIR /usr/include
     CACHE STRING "Pybind11 include path")
 
-# installed with Homebrew in Mac
-#set(PYBIND11_INCLUDE_DIR /opt/homebrew/include
-#    CACHE STRING "Pybind11 include path")
-
 #set(PYBIND11_INCLUDE_DIR /opt
 #    CACHE STRING "Pybind11 include path")
